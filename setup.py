7e0177637e5b03af920249992d1e67eba02a4010
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-05-06 10:48:00 +1000

    Fixed version in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ URL = 'https://github.com/GeoscienceAustralia/dea-waterbodies'
 EMAIL = 'dea@ga.gov.au'
 AUTHOR = 'Geoscience Australia'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -31,12 +30,9 @@ long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
+project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    exec(f.read(), about)
 
 setup(
     name=NAME,
