a7c25d24a5bbaad28a802ffde9b07e4281a1bd72
Matthew Alger <matthew.alger@gmail.com>
2021-05-10 14:14:04 +1000

    Update setup_test_datacube.sh

--- setup_test_datacube.sh
+++ setup_test_datacube.sh
@@ -5,8 +5,9 @@ export DATACUBE_CONFIG_PATH=.test_datacube.conf
 datacube system init
 # Add product definitions
 # WOfS
-https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/master/digitalearthau/config/products/wofs_albers.odc-product.yaml
-https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/master/digitalearthau/config/products/wofs_filtered_summary.odc-product.yaml
+datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/eo3_landsat_ard.odc-type.yaml
+datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/master/digitalearthau/config/products/wofs_albers.odc-product.yaml
+datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/master/digitalearthau/config/products/wofs_filtered_summary.odc-product.yaml
 
-# Index one of each ARD product (5, 7 and 8)
+# Index one WOfS tile
 s3-to-dc "s3://dea-public-data/WOfS/filtered_summary/v2.1.0/combined/x_15/y_-40/wofs_filtered_summary_15_-40.yaml" --no-sign-request --skip-lineage wofs_filtered_summary
