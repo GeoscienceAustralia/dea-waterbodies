7d3a79136ddde8fffc23ecb3b6f44cbce48bc952
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-05-12 09:20:20 +1000

    Added coastline

--- setup_test_datacube.sh
+++ setup_test_datacube.sh
@@ -10,7 +10,11 @@ datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/digi
 datacube product add https://explorer.sandbox.dea.ga.gov.au/products/wofs_albers.odc-product.yaml
 datacube product add https://explorer.sandbox.dea.ga.gov.au/products/wofs_filtered_summary.odc-product.yaml
 datacube product add https://explorer.sandbox.dea.ga.gov.au/products/wofs_summary.odc-product.yaml
+# Coastline
+datacube product add https://explorer.sandbox.dea.ga.gov.au/products/geodata_coast_100k.odc-product.yaml
 
-# Index one WOfS tile
+# Index one WOfS tile (Belconnen)
 s3-to-dc 's3://dea-public-data/WOfS/filtered_summary/v2.1.0/combined/x_15/y_-40/*.yaml' --no-sign-request --skip-lineage 'wofs_filtered_summary'
 s3-to-dc 's3://dea-public-data/WOfS/summary/v2.1.0/combined/x_15/y_-40/*.yaml' --no-sign-request --skip-lineage 'wofs_summary'
+# Coastline tile
+s3-to-dc 's3://dea-public-data/projects/geodata_coast_100k/v2004/x_15/y_-40/*.yaml' --no-sign-request --skip-lineage 'geodata_coast_100k'
