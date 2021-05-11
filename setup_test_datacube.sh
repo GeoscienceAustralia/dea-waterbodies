279a3a14a01e0ad96f6c9d240a008576e89fbdff
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-05-11 15:57:59 +1000

    fixed datacube add

--- setup_test_datacube.sh
+++ setup_test_datacube.sh
@@ -12,5 +12,5 @@ datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digit
 datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/master/digitalearthau/config/products/wofs_summary.odc-product.yaml
 
 # Index one WOfS tile
-s3-to-dc "s3://dea-public-data/WOfS/filtered_summary/v2.1.0/combined/x_15/y_-40/wofs_filtered_summary_15_-40.yaml" --no-sign-request --skip-lineage wofs_filtered_summary
-s3-to-dc "s3://dea-public-data/WOfS/summary/v2.1.0/combined/x_15/y_-40/WOFS_3577_15_-40_summary.yaml" --no-sign-request --skip-lineage wofs_summary
+s3-to-dc 's3://dea-public-data/WOfS/filtered_summary/v2.1.0/combined/x_15/y_-40/*.yaml' --no-sign-request --skip-lineage 'wofs_filtered_summary'
+s3-to-dc 's3://dea-public-data/WOfS/summary/v2.1.0/combined/x_15/y_-40/*.yaml' --no-sign-request --skip-lineage 'wofs_summary'
