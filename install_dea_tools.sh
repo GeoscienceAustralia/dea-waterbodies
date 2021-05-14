af6b4aa5de53519ab269bf9d1dbaa43b35bb4da8
Matthew Alger <matthew.alger@gmail.com>
2021-05-14 15:21:36 +1000

    Create install_dea_tools.sh

---
+++ install_dea_tools.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 --single-branch --no-checkout https://github.com/GeoscienceAustralia/dea-notebooks.git
+cd dea-notebooks
+git sparse-checkout init
+git sparse-checkout set Tools
+git checkout develop
+git sparse-checkout disable
+pip install -e Tools
+cd ..
