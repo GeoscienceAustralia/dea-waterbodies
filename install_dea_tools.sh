ed4c4f6374ba9fddeb0c8d1c7d0d5c2261d8680f
Matthew Alger <matthew.alger@gmail.com>
2021-05-14 15:41:28 +1000

    Update install_dea_tools.sh

--- install_dea_tools.sh
+++ install_dea_tools.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 --single-branch --no-checkout https://github.com/GeoscienceAustralia/dea-notebooks.git
-cd dea-notebooks
-git sparse-checkout init
-git sparse-checkout set Tools
-git checkout develop
-git sparse-checkout disable
+svn export https://github.com/GeoscienceAustralia/dea-notebooks/trunk/Tools
 pip install -e Tools --extra-index-url="https://packages.dea.ga.gov.au"
-cd ..
