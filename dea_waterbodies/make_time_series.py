d933569979200638e7ec8b6d01326a7d29d10356
Matthew Alger <matthew.alger@gmail.com>
2021-06-04 14:51:33 +1000

    Update make_time_series.py

--- dea_waterbodies/make_time_series.py
+++ dea_waterbodies/make_time_series.py
@@ -24,8 +24,7 @@ import click
 @click.argument('config_file')
 @click.option('--part', type=int)
 @click.option('--chunks', type=int)
-@click.option('--size', required=False, type=click.Choice(['SMALL', 'HUGE', 'ALL']))
-def main(config_file, part=1, chunks=1, size='ALL'):
+def main(config_file, part=1, chunks=1):
     """Make the waterbodies time series."""
     # Do the import here so that the CLI is fast, because this import is sloooow.
     import dea_waterbodies.waterbody_timeseries_functions as dw_wtf
@@ -34,7 +33,7 @@ def main(config_file, part=1, chunks=1, size='ALL'):
 
     num_chunks = chunks
 
-    assert size.isupper()
+    assert config_dict['size'].isupper()
 
     # the part and num_chunks arguments are for when you are running a large job in parallel
     if part != 1:
@@ -44,8 +43,7 @@ def main(config_file, part=1, chunks=1, size='ALL'):
         print(f'Splitting into {num_chunks} chunks')
 
     # not used if using huge mem
-    config_dict['size'] = size
-    print(config_dict['size'])
+    print('Size:', config_dict['size'])
 
     #Open the shapefile and get the list of polygons
     shapes_subset, crs, id_field = dw_wtf.get_shapefile_list(config_dict, part, num_chunks)
