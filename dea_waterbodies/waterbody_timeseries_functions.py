2b96284fe95847b3ca5f39ab9b32a8802d71fcba
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-06-10 08:58:55 +1000

    set logging to INFO

--- dea_waterbodies/waterbody_timeseries_functions.py
+++ dea_waterbodies/waterbody_timeseries_functions.py
@@ -14,7 +14,7 @@ from shapely import geometry as shapely_geom
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
 logger = logging.getLogger(__name__)
 
 
