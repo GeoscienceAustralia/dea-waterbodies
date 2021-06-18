6c1833df790fd252a8389949cc985adfc2a7b624
Matthew Alger <matthew.alger@gmail.com>
2021-06-18 14:18:03 +1000

    Update setup_test_datacube.sh

--- setup_test_datacube.sh
+++ setup_test_datacube.sh
@@ -8,7 +8,7 @@ docker-compose exec -T index datacube metadata add "$METADATA_CATALOG"
 # Index products we care about for dea-waterbodies
 docker-compose exec -T index wget "$PRODUCT_CATALOG" -O product_list.csv
 docker-compose exec -T index bash -c "tail -n+2 product_list.csv | grep 'wofs_albers\|wofs_filtered_summary\|wofs_summary' | awk -F , '{print \$2}' | xargs datacube -v product add"
-datacube product add https://explorer.sandbox.dea.ga.gov.au/products/geodata_coast_100k.odc-product.yaml
+docker-compose exec -T index bash -c "datacube product add https://explorer.sandbox.dea.ga.gov.au/products/geodata_coast_100k.odc-product.yaml"
 
 # Index WOfS and Coastline
 cat > index_tiles.sh <<EOF
