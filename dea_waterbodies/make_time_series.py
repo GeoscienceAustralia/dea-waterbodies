c4ac609aba2bb5e23b2e0e51661def75c1d14968
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-06-30 10:32:41 +1000

    Finished first pass of CLI main function

--- dea_waterbodies/make_time_series.py
+++ dea_waterbodies/make_time_series.py
@@ -18,6 +18,7 @@ Geoscience Australia - 2021
 
 import configparser
 import logging
+from pathlib import Path
 import sys
 
 import click
@@ -29,7 +30,7 @@ logging.getLogger("botocore.credentials").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-def process_config(config_file):
+def process_config(config_file: Path) -> dict:
     config = configparser.ConfigParser()
     config_dict = {}
     config.read(config_file)
@@ -82,6 +83,71 @@ def process_config(config_file):
 
     return config_dict
 
+
+def get_crs(shapefile_path: Path) -> geometry.CRS:
+    from datacube.utils import geometry
+    import fiona
+    with fiona.open(shapefile_path) as shapes:
+        crs = geometry.CRS(shapes.crs_wkt)
+    return crs
+
+
+def guess_id_field(shapefile_path: Path) -> str:
+    import fiona
+    with fiona.open(shapefile_path) as shapes:
+        row = next(iter(shapes))
+        keys = set(row['properties'].keys())
+    possible_guesses = [
+        # In order of preference.
+        'UID', 'WB_ID', 'FID_1', 'FID', 'ID', 'OBJECTID',
+    ]
+    for guess in possible_guesses:
+        if guess in keys:
+            return guess
+    raise ValueError(
+        'Couldn\'t find an ID field in {}'.format(keys))
+
+
+def get_shapes(config_dict: dict, wb_ids: [str], id_field: str) -> [dict]:
+    import fiona
+    output_dir = config_dict['output_dir']
+
+    # If missing_only, remove waterbodies that already exist.
+    if config_dict['missing_only']:
+        logger.info("Filtering waterbodies with existing outputs")
+        # NOTE(MatthewJA): I removed references to processed_file here -
+        # I think it should be captured later. If this induces a bug,
+        # here's a great place to start looking.
+        # TODO(MatthewJA): Use Paths earlier on and don't convert here.
+        output_dir = Path(config_dict['output_dir'])
+        missing_list = []
+        for id_ in wb_ids:
+            out_path = output_dir / id_[:4] / f'{id}.csv'
+            if out_path.exists():
+                continue
+            
+            missing_list.append(id_)
+        wb_ids = missing_list
+
+        logger.info(
+            f'{len(missing_list)} missing polygons to process')
+    
+    # Filter the list of shapes to include only specified polygons,
+    # possibly constrained to a state.
+    filtered_shapes = []
+    wb_ids = set(wb_ids)  # for quick membership lookups
+    with fiona.open(config_dict['shape_file']) as shapes:
+        for shape in shapes:
+            if shape['properties'][id_field] not in wb_ids:
+                continue
+            
+            if 'filter_state' in config_dict and shape['properties']['STATE'] != config_dict['filter_state']:
+                continue
+                
+            filtered_shapes.append(shape)
+
+    return filtered_shapes, id_field
+
 @click.command()
 @click.argument('ids', required=False)
 @click.option('--config', '-c', type=click.Path())
@@ -100,6 +166,8 @@ def main(ids, config, shapefile, start, end, size,
          missing_only, skip, time_span, output, state,
          no_mask_obs):
     """Make the waterbodies time series."""
+
+    # TODO(MatthewJA): Read ids from stdin if necessary.
 
     # If we've specified a config file, load it in.
     if config:
@@ -153,41 +221,38 @@ def main(ids, config, shapefile, start, end, size,
         raise click.ClickException('If --start is specified then --end must also be specified')
     if not config_dict['start_dt'] and config_dict['end_date']:
         raise click.ClickException('If --end is specified then --start must also be specified')
+    # These comparisons should probably be case-insensitive anyway, but
+    # confirm here just to be sure.
+    assert config_dict['size'].isupper()
 
     # Do the import here so that the CLI is fast, because this import is sloooow.
     import dea_waterbodies.waterbody_timeseries_functions as dw_wtf
 
-    num_chunks = chunks
-
-    assert config_dict['size'].isupper()
-
-    # the part and num_chunks arguments are for when you are running a large job in parallel
-    if part != 1:
-        print(f'Working on chunk {part}')
-
-    if num_chunks != 1:
-        print(f'Splitting into {num_chunks} chunks')
-
-    # not used if using huge mem
-    print('Size:', config_dict['size'])
-
-    #Open the shapefile and get the list of polygons
-    shapes_subset, crs, id_field = dw_wtf.get_shapefile_list(config_dict, part, num_chunks)
+    # Get the CRS from the shapefile.
+    crs = get_crs(config_dict['shape_file'])
     config_dict['crs'] = crs
+
+    # Guess the ID field.
+    id_field = guess_id_field(config_dict['shape_file'])
     config_dict['id_field'] = id_field
 
+    # Open the shapefile and get the list of polygons.
+    shapes = get_shapes(config_dict, ids, id_field)
+
+    # TODO(MatthewJA): Output all the configuration settings in a better way.
     print('config', config_dict)
 
-    # Loop through the polygons and write out a csv of waterbody percentage area full and wet pixel count
-    # process each polygon. attempt each polygon 2 times
-    for i, shapes in enumerate(shapes_subset):
+    # Loop through the polygons and write out a CSV of wet percentage,
+    # wet area, and wet pixel count.
+    # Attempt each polygon 2 times.
+    for i, shape in enumerate(shapes):
         logger.info('Processing {} ({}/{})'.format(
-            shapes['properties'][config_dict['id_field']],
+            shape['properties'][id_field],
             i + 1,
-            len(shapes_subset)))
-        result = dw_wtf.generate_wb_timeseries(shapes, config_dict)
+            len(shapes)))
+        result = dw_wtf.generate_wb_timeseries(shape, config_dict)
         if not result:
-            result = dw_wtf.generate_wb_timeseries(shapes, config_dict)
+            result = dw_wtf.generate_wb_timeseries(shape, config_dict)
 
 
 if __name__ == "__main__":
