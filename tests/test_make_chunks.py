4499f6a64a1f138d2200109214a1d0c7bbdf0357
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-07-08 13:03:46 +1000

    new vs wraps??

--- tests/test_make_chunks.py
+++ tests/test_make_chunks.py
@@ -32,7 +32,7 @@ SHAPEFILE = {TEST_SHP}
 
 
 def test_get_dbf_from_config(config_path):
-    with mock.patch('urllib.request.urlopen', new=open):
+    with mock.patch('urllib.request.urlopen', wraps=open):
         dbf_path = make_chunks.get_dbf_from_config(config_path)
     assert dbf_path.parent == TEST_SHP.parent
     assert dbf_path.stem == TEST_SHP.stem
