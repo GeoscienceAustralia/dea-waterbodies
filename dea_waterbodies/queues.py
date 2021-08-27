ea919a527983ec83e0745ad1c0d57e515bd1570b
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-27 12:50:53 +1000

    Added queue pusher

--- dea_waterbodies/queues.py
+++ dea_waterbodies/queues.py
@@ -6,10 +6,26 @@ Geoscience Australia
 """
 
 import json
+import logging
 
 import boto3
 from botocore.config import Config
 import click
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_queue(queue_name: str):
+    """
+    Return a queue resource by name, e.g., alex-really-secret-queue
+
+    Cribbed from odc.algo.
+    """
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+    return queue
+
 
 
 def verify_name(name):
@@ -74,6 +90,45 @@ def delete(name):
     arn = queue.attributes['QueueArn']
     queue.delete()
     return arn
+
+
+@cli.command()
+@click.option('--txt', type=click.Path(), required=True,
+              help='REQUIRED. Path to TXT file to push to queue.')
+@click.option('--queue', required=True,
+              help='REQUIRED. Queue name to push to.')
+def push_to_queue(txt, queue):
+    """
+    Push lines of a text file to a SQS queue.
+    """
+    # Cribbed from datacube-alchemist
+    alive_queue = get_queue(queue)
+
+    def post_messages(messages, count):
+        alive_queue.send_messages(Entries=messages)
+        logger.info(f"Added {count} messages...")
+        return []
+
+    count = 0
+    messages = []
+    logger.info("Adding messages...")
+    with open(txt) as file:
+        ids = [line.strip() for line in file]
+    logger.debug(f'Adding IDs {ids}')
+    for id_ in ids:
+        message = {
+            "Id": str(count),
+            "MessageBody": str(id_),
+        }
+        messages.append(message)
+
+        count += 1
+        if count % 10 == 0:
+            messages = post_messages(messages, count)
+
+    # Post the last messages if there are any
+    if len(messages) > 0:
+        post_messages(messages, count)
 
 
 if __name__ == "__main__":
